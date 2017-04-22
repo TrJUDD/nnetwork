e7b1d202fdf8087f310c1bae9f720b6cdd6e65d1
TristanJUDD <tristan.judd@ensae.fr>
2017-04-22 09:17:35 +0200

    ok

--- src/rcpp_hello_world.cpp
+++ src/rcpp_hello_world.cpp
@@ -1,13 +1,14 @@
 
-#include <Rcpp.h>
-using namespace Rcpp;
+#include <RcppArmadillo.h>
+
+// [[Rcpp::depends(RcppArmadillo)]]
 
 // [[Rcpp::export]]
-List rcpp_hello_world() {
+Rcpp::List rcpp_hello_world() {
 
-    CharacterVector x = CharacterVector::create( "foo", "bar" )  ;
-    NumericVector y   = NumericVector::create( 0.0, 1.0 ) ;
-    List z            = List::create( x, y ) ;
+    Rcpp::CharacterVector x = Rcpp::CharacterVector::create( "foo", "bar" )  ;
+    Rcpp::NumericVector y   = Rcpp::NumericVector::create( 0.0, 1.0 ) ;
+    Rcpp::List z            = Rcpp::List::create( x, y ) ;
 
     return z ;
 }
